1d1f6da3a5872ac6d5a76f8353720b8a82822a3d
hzjanger <1305696249@qq.com>
2019-10-07 15:51:53 +0800

    修改登录组件信息

--- src/app/layout/main/login/login.component.ts
+++ src/app/layout/main/login/login.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import {UserService} from '../../../service/user.service';
 import {User} from '../../../entity/user';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {Router} from '@angular/router';
+import {Result} from "../../../entity/comment/Result";
+import {ResultCode} from "../../../entity/comment/ResultCode";
+import {MatSnackBar} from "@angular/material";
+import {LocalStorageInfo} from "../../../utils/LocalStorageInfo";
 
 export class ddd {
   id:number;
@@ -16,36 +20,76 @@ export class ddd {
 export class LoginComponent implements OnInit {
 
   user: User = new User();
-  constructor(private userService: UserService, private router: Router) { }
 
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private fb: FormBuilder,
+    private snackBar: MatSnackBar
+  ) { }
 
-  login = new FormGroup({
-    password: new FormControl(''),
-    userAccount: new FormControl(''),
-  });
+  /**
+   * 登录表动控件
+   */
+  loginGroup: FormGroup;
+
   ngOnInit() {
+    this.loginGroup = this.fb.group({
+      account: [null, [Validators.required, Validators.email]],
+      password: [null, Validators.required]
+    });
   }
-
-
-
-  caa: ddd;
 
   /**
    * 登录
    */
   onSubmit() {
-    this.caa = new ddd();
-    let c = new ddd();
-    this.user.user_account = this.login.value.userAccount;
-    this.user.user_password = this.login.value.password;
-    this.userService.login(this.user)
-      .subscribe(data => {
-        if (data) {
-          localStorage.setItem('userId', String(data));
-          this.router.navigate(['/']);
-        } else {
-          alert('用户名或密码错误');
-        }
-      })
+    if (this.loginGroup.valid) {
+      this.userService.login(this.loginGroup.value)
+        .subscribe((data: Result) => {
+          if (data.success && data.code === ResultCode.loginSuccess) {
+            //将用户的信息保存在localStorage中
+            LocalStorageInfo.userInfo = data.data;
+            this.snackBar.open('登录成功', '关闭', {
+              duration: 1000
+            });
+            this.router.navigate(['/']);
+          } else {
+            this.snackBar.open(data.message, "关闭", {
+              duration: 1000
+            });
+          }
+        });
+    }
   }
+
+  /**
+   * 用户密码的表单控件
+   */
+  get password(): FormControl {
+    return this.loginGroup.get('password') as FormControl;
+  }
+
+  /**
+   * 用户账户的表单控件
+   */
+  get account(): FormControl {
+    return this.loginGroup.get('account') as FormControl;
+  }
+
+  /**
+   * 密码输入的错误信息
+   */
+  getPasswordError(): string {
+    return this.password.hasError('required') ? '输入不能为空' : '';
+  }
+
+  /**
+   * 账户输入的错误信息
+   */
+  getAccountError(): string {
+    return this.account.hasError('required') ? '输入不能为空' :
+      this.account.hasError('email') ? '账户的格式错误': '';
+  }
+
 }
